3fd13e57f3d483fb099c054c0ea8cf90841eaf88
marlo <apple@marlo-MacBook-Pro.local>
2019-04-24 19:59:21 -0400

    Updates

---
+++ precache-manifest.739e5fdb7ca1c734bf96bddddad773ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78801230158acc5d2804",
+    "url": "/Gym-todo/static/css/main.f2ed4cf4.chunk.css"
+  },
+  {
+    "revision": "78801230158acc5d2804",
+    "url": "/Gym-todo/static/js/main.3a993dd4.chunk.js"
+  },
+  {
+    "revision": "578393e6a34a62d91e57",
+    "url": "/Gym-todo/static/js/runtime~main.11607b58.js"
+  },
+  {
+    "revision": "94c3165186f215582bb2",
+    "url": "/Gym-todo/static/js/2.c37d2ede.chunk.js"
+  },
+  {
+    "revision": "45e54f1d2c70bc641b60f2b811e63fc1",
+    "url": "/Gym-todo/index.html"
+  }
+];
